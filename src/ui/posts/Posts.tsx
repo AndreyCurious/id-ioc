d9701068941eaac4e74f8493e8a96b46039af586
Andrey Didenko <ded14181998@gmail.com>
2024-09-19 15:33:38 +0500

    fixes

--- src/ui/posts/Posts.tsx
+++ src/ui/posts/Posts.tsx
@@ -7,12 +7,12 @@ import { Post } from '../../domain/Post';
 import { PostApi } from '../../Infrastructure/api/PostApi';
 import { PostService } from '../../application/services/postService';
 import { useParams } from 'react-router-dom';
+import container from '../../container';
 
 function Posts() {
   const store = useStore();
   const { posts, updatePosts } = store;
-  const postService = new PostService();
-  const postApi = new PostApi(postService);
+  const postApi = container.get(PostApi);
   const userId = useParams().id;
   useLayoutEffect(() => {
     userId && postApi.getPosts(userId).then((res) => updatePosts(res));
